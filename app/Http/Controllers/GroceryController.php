072d13f2817b63cbfdab73e53624fdf804961ec2
Wilco Menge <wilcomenge@gmail.com>
2025-01-26 21:33:17 +0100

    Grocery model + controller

---
+++ app/Http/Controllers/GroceryController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Grocery;
+use Illuminate\Http\Request;
+
+class GroceryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(Grocery::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // TODO: map/validate/be generally very paranoid of user input!
+        $grocery = new Grocery;
+        $grocery->name = $request->name;
+        $grocery->save();
+        return response()->json([$grocery, 201]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(int $id)
+    {
+        $grocery = Grocery::find($id);
+
+        if (!empty($grocery)) {
+            return response()->json($grocery);
+        } else {
+            return response()->json(["Grocery not found", 404]);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, int $id)
+    {
+        $grocery = Grocery::find($id);
+
+        if (!empty($grocery)) {
+            // TODO: map/validate/be generally very paranoid of user input!
+            if (!is_null($request->name)) $grocery->name = $request->name;
+            $grocery->save();
+            return response()->json([$grocery, 200]);
+        } else {
+            return response()->json(["Grocery not found", 404]);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(int $id)
+    {
+        $grocery = Grocery::find($id);
+
+        if (!empty($grocery)) {
+            $grocery->delete();
+            return response()->json(["Grocery deleted", 202]);
+        } else {
+            return response()->json(["Grocery not found", 404]);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GroceryController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/groceries', [GroceryController::class, 'index']);
+Route::get('/groceries/{id}', [GroceryController::class, 'show']);
+Route::post('/groceries', [GroceryController::class, 'store']);
+Route::put('/groceries/{id}', [GroceryController::class, 'update']); // TODO: Patch?
+Route::delete('/groceries/{id}', [GroceryController::class, 'destroy']);
